f57fdb3e848a680974199bcfe727f79a44e0742b
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2025-04-08 00:45:04 +0530

    Update Handcricket.java

--- Handcricket.java
+++ Handcricket.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class Handcricket {
     static Scanner sc=new Scanner(System.in);
+    static Random r=new Random();
     int toss(String a)
     {
-        int t=(int)(Math.random()*(1-0+1));
+        int t=r.nextInt(0,2);
         if(a.equalsIgnoreCase("heads")&&t==0)
         return 1;
         else if(a.equalsIgnoreCase("tails")&&t==1)
@@ -14,40 +15,55 @@ public class Handcricket {
     void superover(int x,int y)
     {
         Handcricket ob=new Handcricket();
-        int ba,bo,sb=0,s=0,w1=y;
+        int ba,bo,sb=0,s=0,w1=y,nbc=0;
         if (x==1)
         {
             System.out.println("Enter the bowls");
+            nbc=0;
             while(w1!=0)
             {
                 bo=sc.nextInt();
-                ba=(int)(Math.random()*(6-0+1));
+                if(bo>6||bo<0){
+                    System.out.println("Enter bowls between 1 to 6");
+                    continue;
+                }
+                ba=r.nextInt(0,7);
                 System.out.println(bo);
                 if(ba==bo)
                 {
                     w1--;
                     System.out.println("HOWZATT!!");
                 }
-                else if (w1<=y-2 && bo==0) 
-                sb+=6;                        
+                else if (bo==0){
+                    nbc++;
+                    if(nbc>=3)
+                    sb+=6;
+                }                        
                 else
                 sb+=ba;
                 System.out.println("Score: "+sb+" - "+(y-w1));
             }
             System.out.println("Target: "+sb+"\nEnter the runs");
-            w1=y;
+            w1=y;nbc=0;
             while(w1!=0)
             {
                 ba=sc.nextInt();
-                bo=(int)(Math.random()*(6-0+1));
+                if(ba>6||ba<0){
+                    System.out.println("Enter the runs between 1 to 6");
+                    continue;
+                }
+                bo=r.nextInt(0,7);
                 System.out.println(ba);
                 if(ba==bo)
                 {
                     w1--;
                     System.out.println("HOWZATT!!");
                 }
-                else if (w1<=y-2 && bo==0) 
-                s+=6;                        
+                else if (bo==0){
+                    nbc++;
+                    if(nbc>=3)
+                    s+=6;
+                }                        
                 else
                 s+=ba;
                 System.out.println("Score: "+s+" - "+(y-w1));
@@ -66,36 +82,51 @@ public class Handcricket {
         }
         else{
             System.out.println("Enter the runs");
+            nbc=0;
             while(w1!=0)
             {
                 ba=sc.nextInt();
-                bo=(int)(Math.random()*(6-0+1));
+                if(ba>6||ba<0){
+                    System.out.println("Enter the runs between 1 to 6");
+                    continue;
+                }
+                bo=r.nextInt(0,7);
                 System.out.println(bo);
                 if(ba==bo)
                 {
                     w1--;
                     System.out.println("HOWZATT!!");
                 }
-                else if (w1<=y-2 && bo==0) 
-                sb+=6;                        
+                else if (bo==0){
+                    nbc++;
+                    if(nbc>=3)
+                    sb+=6;
+                }                        
                 else
                 sb+=ba;
                 System.out.println("Score: "+sb+" - "+(y-w1));
             }
-            System.out.println("Target: "+sb+"\nEnter the bowls");
-            w1=y;
+            System.out.println("Target: "+(sb+1)+"\nEnter the bowls");
+            w1=y;nbc=0;
             while(w1!=0)
             {
                 bo=sc.nextInt();
-                ba=(int)(Math.random()*(6-0+1));
+                if(bo>6||bo<0){
+                    System.out.println("Enter bowls between 1 to 6");
+                    continue;
+                }
+                ba=r.nextInt(0,7);
                 System.out.println(ba);
                 if(ba==bo)
                 {
                     w1--;
                     System.out.println("HOWZATT!!");
                 }
-                else if (w1<=y-2 && bo==0) 
-                s+=6;                        
+                else if (bo==0){
+                    nbc++;
+                    if(nbc>=3)
+                    s+=6;
+                }                       
                 else
                 s+=ba;
                 System.out.println("Score: "+s+" - "+(y-w1));
@@ -117,7 +148,7 @@ public class Handcricket {
         System.out.println("Enter \'Heads\' or \'Tails\'");
         String c=sc.next();
         Handcricket ob=new Handcricket();
-        int tr=ob.toss(c),ch,w,ba,bo,sb=0,s=0,w1;
+        int tr=ob.toss(c),ch,w,ba,bo,sb=0,s=0,w1,nbc=0;//sb is sum of computer runs
         if(tr==1)
         {
             System.out.println("You won the toss\nChoose by entering \'Bat\' or \'Bowl\'");
@@ -128,6 +159,7 @@ public class Handcricket {
             System.out.println("You chose to "+c+"\nWickets: "+w);
             if(c.equalsIgnoreCase("Bat"))
             {
+                nbc=0;
                 System.out.println("Enter the runs");
                 while(w1!=0)
                 {
@@ -136,21 +168,24 @@ public class Handcricket {
                         System.out.println("Enter the runs between 1 to 6");
                         continue;
                     }
-                    bo=(int)(Math.random()*(6-0+1));
+                    bo=r.nextInt(0,7);
                     System.out.println(bo);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    sb+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        sb+=6;
+                    }                        
                     else
                     sb+=ba;
                     System.out.println("Score: "+sb+" - "+(w-w1));
                 }
-                System.out.println("Target: "+sb+"\nEnter the bowls");
-                w1=w;
+                System.out.println("Target: "+(sb+1)+"\nEnter the bowls");
+                w1=w;nbc=0;
                 while(w1!=0)
                 {
                     bo=sc.nextInt();
@@ -158,15 +193,18 @@ public class Handcricket {
                         System.out.println("Enter bowls between 1 to 6");
                         continue;
                     }
-                    ba=(int)(Math.random()*(6-0+1));
+                    ba=r.nextInt(0,7);
                     System.out.println(ba);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    s+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        s+=6;
+                    }                      
                     else
                     s+=ba;
                     System.out.println("Score: "+s+" - "+(w-w1));
@@ -184,6 +222,7 @@ public class Handcricket {
             }
             else if(c.equalsIgnoreCase("Bowl"))
             {
+                nbc=0;
                 System.out.println("Enter the bowls");
                 while(w1!=0)
                 {
@@ -192,21 +231,24 @@ public class Handcricket {
                         System.out.println("Enter bowls between 1 to 6");
                         continue;
                     }
-                    ba=(int)(Math.random()*(6-0+1));
+                    ba=r.nextInt(0,7);
                     System.out.println(ba);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    sb+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        sb+=6;
+                    }                        
                     else
                     sb+=ba;
                     System.out.println("Score: "+sb+" - "+(w-w1));
                 }
                 System.out.println("Target: "+sb+"\nEnter the runs");
-                w1=w;
+                w1=w;nbc=0;
                 while(w1!=0)
                 {
                     ba=sc.nextInt();
@@ -214,15 +256,18 @@ public class Handcricket {
                         System.out.println("Enter the runs between 1 to 6");
                         continue;
                     }
-                    bo=(int)(Math.random()*(6-0+1));
+                    bo=r.nextInt(0,7);
                     System.out.println(bo);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    s+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        s+=6;
+                    }                        
                     else
                     s+=ba;
                     System.out.println("Score: "+s+" - "+(w-w1));
@@ -242,11 +287,12 @@ public class Handcricket {
         else
         {
             System.out.println("You lost the toss");
-            ch=(int)(Math.random()*(1-0+1));
-            w=(int)(Math.random()*(10-1+1));
+            ch=r.nextInt(1,3);
+            w=r.nextInt(1,11);
             w1=w;
             if(ch==1)
             {
+                nbc=0;
                 System.out.println("Computer chose to bat\nWickets: "+w);
                 System.out.println("Enter the bowls");
                 while(w1!=0)
@@ -256,21 +302,24 @@ public class Handcricket {
                         System.out.println("Enter bowls between 1 to 6");
                         continue;
                     }
-                    ba=(int)(Math.random()*(6-0+1));
+                    ba=r.nextInt(0,7);
                     System.out.println(ba);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    sb+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        sb+=6;
+                    }                        
                     else
                     sb+=ba;
                     System.out.println("Score: "+sb+" - "+(w-w1));
                 }
                 System.out.println("Target: "+sb+"\nEnter the runs");
-                w1=w;
+                w1=w;nbc=0;
                 while(w1!=0)
                 {
                     ba=sc.nextInt();
@@ -278,15 +327,18 @@ public class Handcricket {
                         System.out.println("Enter the runs between 1 to 6");
                         continue;
                     }
-                    bo=(int)(Math.random()*(6-0+1));
+                    bo=r.nextInt(0,7);
                     System.out.println(bo);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    s+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        s+=6;
+                    }                        
                     else
                     s+=ba;
                     System.out.println("Score: "+s+" - "+(w-w1));
@@ -304,6 +356,7 @@ public class Handcricket {
             }
             else
             {
+                nbc=0;
                 System.out.println("Computer chose to bowl\nWickets: "+w);
                 System.out.println("Enter the runs");
                 while(w1!=0)
@@ -313,21 +366,24 @@ public class Handcricket {
                         System.out.println("Enter the runs between 1 to 6");
                         continue;
                     }
-                    bo=(int)(Math.random()*(6-0+1));
+                    bo=r.nextInt(0,7);
                     System.out.println(bo);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    sb+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        sb+=6;
+                    }                        
                     else
                     sb+=ba;
                     System.out.println("Score: "+sb+" - "+(w-w1));
                 }
-                System.out.println("Target: "+sb+"\nEnter the bowls");
-                w1=w;
+                System.out.println("Target: "+(sb+1)+"\nEnter the bowls");
+                w1=w;nbc=0;
                 while(w1!=0)
                 {
                     bo=sc.nextInt();
@@ -335,15 +391,18 @@ public class Handcricket {
                         System.out.println("Enter bowls between 1 to 6");
                         continue;
                     }
-                    ba=(int)(Math.random()*(6-0+1));
+                    ba=r.nextInt(0,7);
                     System.out.println(ba);
                     if(ba==bo)
                     {
                         w1--;
                         System.out.println("HOWZATT!!");
                     }
-                    else if (w1<=w-2 && bo==0) 
-                    s+=6;                        
+                    else if (bo==0){
+                        nbc++;
+                        if(nbc>=3)
+                        s+=6;
+                    }                       
                     else
                     s+=ba;
                     System.out.println("Score: "+s+" - "+(w-w1));
